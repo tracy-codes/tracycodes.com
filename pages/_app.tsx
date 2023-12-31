58108d200b84c2249ef2d749f8b15661b253cdec
Tracy Adams <31976538+tracy-codes@users.noreply.github.com>
2023-12-30 22:16:30 -0800

    Add Fathom analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,34 @@ import "nextra-theme-blog/style.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import "../styles/main.css";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import * as Fathom from "fathom-client";
 
 export default function App({ Component, pageProps }: AppProps) {
+    const router = useRouter();
+
+    useEffect(() => {
+        // Initialize Fathom when the app loads
+        // Example: yourdomain.com
+        //  - Do not include https://
+        //  - This must be an exact match of your domain.
+        //  - If you're using www. for your domain, make sure you include that here.
+        Fathom.load("GTYMHZJT", {
+            includedDomains: ["tracycodes.com"],
+        });
+
+        function onRouteChangeComplete() {
+            Fathom.trackPageview();
+        }
+        // Record a pageview when route changes
+        router.events.on("routeChangeComplete", onRouteChangeComplete);
+
+        // Unassign event listener
+        return () => {
+            router.events.off("routeChangeComplete", onRouteChangeComplete);
+        };
+    }, []);
     return (
         <>
             <Head>
